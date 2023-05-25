9ef1194fe59ed630703d3c5eb1369e03a186b647
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-05-25 09:34:08 +0100

    updated

--- src/config/production.js
+++ src/config/production.js
@@ -3,9 +3,9 @@ dotenv.config()
 
 
 
-export const production = {
-  mongodb_connection_url: process.env.PRODUCTION_MONGODB_CONNECTION_URL,
-  bycrypt_salt_round: +process.env.PRODUCTION_BCRYPT_SALT_ROUND,
-  jwt_secret_key: process.env.PRODUCTION_JWT_SECRET,
-  port:+process.env.PORT
-}
+// export const production = {
+//   mongodb_connection_url: process.env.PRODUCTION_MONGODB_CONNECTION_URL,
+//   bycrypt_salt_round: +process.env.PRODUCTION_BCRYPT_SALT_ROUND,
+//   jwt_secret_key: process.env.PRODUCTION_JWT_SECRET,
+//   port:+process.env.PORT
+// }
