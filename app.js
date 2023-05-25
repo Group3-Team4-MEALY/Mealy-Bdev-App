dbac4a9f5e2f4e52fe55d53fe64ef4e16991b588
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-05-25 06:00:55 +0100

    updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import {router as restaurantRouter} from "./src/routers/restaurant.route.js"
 
 const app = express()
 
-await mongoose.connect(config.mongodb_connection_url).then(()=> console.log("Database connection established")).catch(e=> console.log("Mongo connection error: ", e.message))
+ mongoose.connect(config.mongodb_connection_url).then(()=> console.log("Database connection established")).catch(e=> console.log("Mongo connection error: ", e.message))
 
 
 const port = config.port || 4000;
