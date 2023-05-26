cc27b8c292f0211a309f0ccc31297888f0bd6b63
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-05-26 13:45:10 +0100

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -81,59 +81,5 @@ export default class UserController {
     })
   }
 
-  // static async sendMail(req, res) {
-  //   const transporter = nodemailer.createTransport({
-  //     service: 'gmail',
-  //     auth: {
-  //       user: 'dmnion4.glowd@gmail.com',
-  //       pass: 'marriedugo'
-  //     }
-  //   });
-  
-  //   const mailOptions = {
-  //     from: 'dmnion4.glowd@gmail.com',
-  //     to: 'estelas.com@gmail.com',
-  //     subject: 'Test Email',
-  //     text: 'This is a test email.'
-  //   };
-  
-  //   await transporter.sendMail(mailOptions);
-  
-  //   res.status(200).json({
-  //     message: 'Email sent successfully',
-  //     status: 'Success'
-  //   });
-  // }1583435
-  
-
-  static async sendMail(req, res) {
-    try {
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        auth: {
-          user: '69997bda3708d4',
-          pass: '0c87103f8547a2'
-        }
-      });
-  
-      const mailOptions = {
-        from: 'dmnion4.glowd@gmail.com',
-        to: 'benitaekeneokikere@gmail.com',
-        subject: 'Test Email',
-        text: 'This is a test email.'
-      };
-  
-      await transporter.sendMail(mailOptions);
-  
-      res.status(200).json({
-        message: 'Email sent successfully',
-        status: 'Success'
-      });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  }
 
 }
